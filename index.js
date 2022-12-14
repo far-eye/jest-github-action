768ff231f032627592ef0c068cf2556c4ecdc155
sarinashish <ashish.sarin@getfareye.com>
2022-12-15 00:36:58 +0530

    v1.15.0

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ try {
   });
   const octokit = getOctokit(token);
   await octokit.rest.checks.create(payload)
-  await octokit.issues.createComment(payload.summary);
+  await octokit.rest.issues.createComment(payload.summary);
 
 } catch (error) {
   core.setFailed(error.message);

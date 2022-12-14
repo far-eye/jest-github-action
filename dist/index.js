bb67d810d0c055cadfb9b495bb50f0248d066c96
sarinashish <ashish.sarin@getfareye.com>
2022-12-14 20:04:58 +0530

    v1.5

--- dist/index.js
+++ dist/index.js
@@ -9693,7 +9693,11 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+
+  // Create jest command
+  const jestCmd = "npm test -- --ci --json --coverage --testLocationInResults --outputFile=report.json";
+  console.log("jestCommand -> ", jestCmd);
+
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -7,7 +7,11 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+
+  // Create jest command
+  const jestCmd = "npm test -- --ci --json --coverage --testLocationInResults --outputFile=report.json";
+  console.log("jestCommand -> ", jestCmd);
+
 } catch (error) {
   core.setFailed(error.message);
 }

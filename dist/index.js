9c853bfa64a1628a91cda05b0c2f6cedf56f28e1
sarinashish <ashish.sarin@getfareye.com>
2022-12-15 18:29:02 +0530

    v1.26.0

--- dist/index.js
+++ dist/index.js
@@ -11043,35 +11043,7 @@ async function runAction() {
         await runJestCmd();
         const results = await readResult();
         console.debug('resuls here', { results: results?.success });
-        if (results) {
-            const payload = {
-                ..._actions_github__WEBPACK_IMPORTED_MODULE_0__.context.repo,
-                head_sha: _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload.pull_request?.head.sha ?? _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.sha,
-                name: "jest-github-action-test",
-                status: "completed",
-                conclusion: results.success ? "success" : "failure",
-                output: {
-                    title: results.success ? "Jest tests passed" : "Jest tests failed",
-                    text: results.success ? "All " + results.numTotalTests + " test cases passed." : results.numFailedTestSuites + " test cases failed out of " + results.numTotalTests,
-                    summary: results.success
-                        ? `${results.numPassedTests} tests passing in ${results.numPassedTestSuites
-                        } suite${results.numPassedTestSuites > 1 ? "s" : ""}.`
-                        : `Failed tests: ${results.numFailedTests}/${results.numTotalTests}. Failed suites: ${results.numFailedTests}/${results.numTotalTestSuites}.`,
-                }
-            }
-            console.debug({ payload });
-            const token = core.getInput('github-token', {
-                required: true,
-            });
-            const octokit = (0,_actions_github__WEBPACK_IMPORTED_MODULE_0__.getOctokit)(token);
-            await octokit.rest.checks.create(payload)
-            const commentPayload = {
-                ..._actions_github__WEBPACK_IMPORTED_MODULE_0__.context.repo,
-                body: payload.output.summary,
-                issue_number: _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload.pull_request?.number ?? 0
-            }
-            await octokit.rest.issues.createComment(commentPayload);
-        }
+        await printResult(results);
     } catch (error) {
         console.log("error->", error.message);
         core.setFailed(error.message)
@@ -11079,7 +11051,7 @@ async function runAction() {
 }
 
 async function runJestCmd() {
-    
+
     try {
         // Create jest command
         const jestCmd = `npm test sortingSaga languageSaga -- --ci --json --coverage --testLocationInResults --outputFile=${TEST_FILE_REPORT}`;
@@ -11099,11 +11071,46 @@ async function readResult() {
         console.log("resultFilePath -> ", resultFilePath);
         results = JSON.parse((0,fs__WEBPACK_IMPORTED_MODULE_3__.readFileSync)(resultFilePath, "utf-8"))
         console.debug({ resultsSuccess: Boolean(results?.success) });
-    } catch(error) {
+    } catch (error) {
         console.log("error->", error.message);
         core.setFailed(error.message)
     } finally {
         return results;
+    }
+}
+
+async function printResult(results) {
+    if (results) {
+        const payload = {
+            ..._actions_github__WEBPACK_IMPORTED_MODULE_0__.context.repo,
+            head_sha: _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload.pull_request?.head.sha ?? _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.sha,
+            name: "jest-github-action-test",
+            status: "completed",
+            conclusion: results.success ? "success" : "failure",
+            output: {
+                title: results.success ? "Jest tests passed" : "Jest tests failed",
+                text: results.success ? "All " + results.numTotalTests + " test cases passed." : results.numFailedTestSuites + " test cases failed out of " + results.numTotalTests,
+                summary: `Test Suites: ${results.numPassedTestSuites} passed, ${numTotalTestSuites} total` 
+                    + '\n' 
+                    + `Tests:       ${results.numPassedTests} passed, ${results.numTotalTests} total`
+            }
+        }
+        console.debug({ payload });
+        const token = core.getInput('github-token', {
+            required: true,
+        });
+        const octokit = (0,_actions_github__WEBPACK_IMPORTED_MODULE_0__.getOctokit)(token);
+        await octokit.rest.checks.create(payload)
+        const commentPayload = {
+            ..._actions_github__WEBPACK_IMPORTED_MODULE_0__.context.repo,
+            body: payload.output.summary,
+            issue_number: _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload.pull_request?.number ?? 0
+        }
+        await octokit.rest.issues.createComment(commentPayload);
+        if(!result?.success) {
+            // Fail action check if all test cases are not successful
+            await core.setFailed("Test cases failing");
+        }
     }
 }
 })();
--- index.js
+++ index.js
@@ -16,35 +16,7 @@ async function runAction() {
         await runJestCmd();
         const results = await readResult();
         console.debug('resuls here', { results: results?.success });
-        if (results) {
-            const payload = {
-                ...context.repo,
-                head_sha: context.payload.pull_request?.head.sha ?? context.sha,
-                name: "jest-github-action-test",
-                status: "completed",
-                conclusion: results.success ? "success" : "failure",
-                output: {
-                    title: results.success ? "Jest tests passed" : "Jest tests failed",
-                    text: results.success ? "All " + results.numTotalTests + " test cases passed." : results.numFailedTestSuites + " test cases failed out of " + results.numTotalTests,
-                    summary: results.success
-                        ? `${results.numPassedTests} tests passing in ${results.numPassedTestSuites
-                        } suite${results.numPassedTestSuites > 1 ? "s" : ""}.`
-                        : `Failed tests: ${results.numFailedTests}/${results.numTotalTests}. Failed suites: ${results.numFailedTests}/${results.numTotalTestSuites}.`,
-                }
-            }
-            console.debug({ payload });
-            const token = core.getInput('github-token', {
-                required: true,
-            });
-            const octokit = getOctokit(token);
-            await octokit.rest.checks.create(payload)
-            const commentPayload = {
-                ...context.repo,
-                body: payload.output.summary,
-                issue_number: context.payload.pull_request?.number ?? 0
-            }
-            await octokit.rest.issues.createComment(commentPayload);
-        }
+        await printResult(results);
     } catch (error) {
         console.log("error->", error.message);
         core.setFailed(error.message)
@@ -52,7 +24,7 @@ async function runAction() {
 }
 
 async function runJestCmd() {
-    
+
     try {
         // Create jest command
         const jestCmd = `npm test sortingSaga languageSaga -- --ci --json --coverage --testLocationInResults --outputFile=${TEST_FILE_REPORT}`;
@@ -72,10 +44,45 @@ async function readResult() {
         console.log("resultFilePath -> ", resultFilePath);
         results = JSON.parse(readFileSync(resultFilePath, "utf-8"))
         console.debug({ resultsSuccess: Boolean(results?.success) });
-    } catch(error) {
+    } catch (error) {
         console.log("error->", error.message);
         core.setFailed(error.message)
     } finally {
         return results;
+    }
+}
+
+async function printResult(results) {
+    if (results) {
+        const payload = {
+            ...context.repo,
+            head_sha: context.payload.pull_request?.head.sha ?? context.sha,
+            name: "jest-github-action-test",
+            status: "completed",
+            conclusion: results.success ? "success" : "failure",
+            output: {
+                title: results.success ? "Jest tests passed" : "Jest tests failed",
+                text: results.success ? "All " + results.numTotalTests + " test cases passed." : results.numFailedTestSuites + " test cases failed out of " + results.numTotalTests,
+                summary: `Test Suites: ${results.numPassedTestSuites} passed, ${numTotalTestSuites} total` 
+                    + '\n' 
+                    + `Tests:       ${results.numPassedTests} passed, ${results.numTotalTests} total`
+            }
+        }
+        console.debug({ payload });
+        const token = core.getInput('github-token', {
+            required: true,
+        });
+        const octokit = getOctokit(token);
+        await octokit.rest.checks.create(payload)
+        const commentPayload = {
+            ...context.repo,
+            body: payload.output.summary,
+            issue_number: context.payload.pull_request?.number ?? 0
+        }
+        await octokit.rest.issues.createComment(commentPayload);
+        if(!result?.success) {
+            // Fail action check if all test cases are not successful
+            await core.setFailed("Test cases failing");
+        }
     }
 }

a3bddcdbb68990937dab816872d3f6f406e398a2
sarinashish <ashish.sarin@getfareye.com>
2022-12-18 22:06:45 +0530

    v1.33.5

--- index.js
+++ index.js
@@ -69,26 +69,27 @@ async function runJestCmd(changedFiles) {
     try {
         // Create jest command
         const changedFiledStr = changedFiles.join(' ');
-        // const jestCmd = `npm test ${changedFiledStr} -- --ci --json --coverage --testLocationInResults --outputFile=${TEST_FILE_REPORT}`;
+        const jestCmd = `npm test ${changedFiledStr} -- --ci --json --coverage --testLocationInResults --outputFile=${TEST_FILE_REPORT}`;
         
         let myOutput = '';
         let myError = '';
 
         const options = {};
-        options.listeners = {
-            stdout: (data) => {
-                myOutput += data;
-            },
-            stderr: (data) => {
-                myError += data.toString();
-            },
-            stdline: data => {
-                console.log({stdline});
-            }
-        };
+        // options.listeners = {
+        //     stdout: (data) => {
+        //         myOutput += data;
+        //     },
+        //     stderr: (data) => {
+        //         myError += data.toString();
+        //     },
+        //     stdline: data => {
+        //         console.log({stdline});
+        //     }
+        // };
+        options.cwd = CWD;
         
         
-        const jestCmd = `npm test --passWithNoTests --listTests --findRelatedTests ${changedFiledStr}`;
+        // const jestCmd = `npm test --listTests --findRelatedTests ${changedFiledStr}`;
         console.log("jestCommand -> ", jestCmd);
         // await exec(jestCmd, [], { cwd: CWD });
         const stdout = await exec(jestCmd, [], options);
